644ea92229be7b00090d17151bd096f65c848ce8
Prashant Jaiswal <87226592+Prashant7197@users.noreply.github.com>
2021-07-21 20:50:22 +0530

    erp1

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if gt IE 9]> <html lang="en" class="ie"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Title -->
+<title>Tekriwal Education Pvt. Ltd.</title>
+<meta name="description" content="Tekwiral Education Online Portal ">
+<meta name="author" content="Mr. Pankaj Tekriwal">
+<meta name="keyword" content="Best Educational Institute of Varanasi, Tekriwal Education Pvt Ltd, BICA, Blitz Institute of Creative Arts, Best Franchisee Institute, Computer Classes in Varanasi, Certification Valid in INDIA">
+	
+<!-- Favicon -->
+<link href="images/favicon.png" rel="icon" type="image/png">
+<link href="images/favicon.png" rel="shortcut icon" type="image/png">
+
+<!-- css file -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<!-- Responsive stylesheet -->
+<link rel="stylesheet" href="css/responsive.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="wrapper">
+<!--  <div class="preloader"></div>-->
+  
+  
+  <!-- Header Styles -->
+ <?php 
+ include "./navpage.php"
+ ?>
+  
+  <!-- Home Design -->
+  <div class="irs-home-slider">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 irs-pmz">
+          <div class="irs-main-slider">
+            <div class="item">
+              <div class="caption animatedParent">
+                <div class="irs-text-one animated fadeInnUp delay-250">Receive a world-class <br>
+                  education in the heart of <br>
+                  the west.</div>
+                <div class="irs-text-three animated fadeInnUp delay-500">
+                  <p>Top rated for combining academic quality and outdoor reacreation.</p>
+                </div>
+                <!--<a href="#" class="btn btn-lg irs-btn-thm irs-home-btn animated fadeInUp delay-750"> <span> Register Today</span> </a> <a href="#" class="btn btn-lg irs-btn-thm2 irs-home-btn animated fadeInUp delay-1000"> <span> Check Courses</span> </a> </div>-->
+                </div>
+                <img class="img-responsive" src="images/bika.jpg" alt="h1.jpg"> </div>
+           
+            <div class="item">
+              <div class="caption three text-center animatedParent">
+                <div class="irs-text-one animated fadeInUp delay-2000">Make a bold decision today <br>
+                  and start a new fresh tomorrow. </div>
+                <div class="irs-text-three animated fadeInUp delay-2250">
+                  <p>Top rated for combining academic quality and outdoor reacreation.</p>
+                </div>
+                <!--<a href="#" class="btn btn-lg irs-btn-thm irs-home-btn animated fadeInUp delay-2250"> <span> Register Today</span> </a> <a href="#" class="btn btn-lg irs-btn-thm irs-home-btn animated fadeInUp delay-2750"> <span> Check Courses</span> </a> </div>-->
+              </div> <img class="img-responsive" src="images/home/h3.jpg" alt="h3.jpg"> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+   
+  <!-- Education Commitment Section -->
+  <section class="irs-commitment-one">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center">
+          <div class="irs-commitment-title text-center">
+            <h2>Commitment to Education</h2>
+            <!--<img class="img-responsive" src="images/item3.png" alt="commitment-title-img.png"> -->
+        </div></div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+          <div class="animatedParent">
+            <div class="irs-commtmnt-column style_one text-left animated growIn delay-250">
+              <div class="irs-cmmt-icon"><span class="flaticon-clothes"></span></div>
+              <div class="irs-cmmt-details">
+                <h3>Four Year Graduation Guarantee</h3>
+                <p>By enrolling in the Four Bear Program, we guarantee you will graduate in four years with a bachelor’s degree.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+          <div class="animatedParent">
+            <div class="irs-commtmnt-column2 text-right animated growIn delay-250">
+              <div class="irs-cmmt-icon"><span class="flaticon-signs"></span></div>
+              <div class="irs-cmmt-details2">
+                <h3>Best Value</h3>
+                <p>A top 100 university based on research, service and social mobility, according to Washington Monthly.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+          <div class="animatedParent">
+            <div class="irs-commtmnt-column text-left animated growIn delay-500">
+              <div class="irs-cmmt-icon"><span class="flaticon-school"></span></div>
+              <div class="irs-cmmt-details">
+                <h3>Small Class Sizes</h3>
+                <p>76% of all undergraduate classes have fewer than 30 students so there´s plenty of room for everyone.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+          <div class="animatedParent">
+            <div class="irs-commtmnt-column2 text-right animated growIn delay-500">
+              <div class="irs-cmmt-icon"><span class="flaticon-art"></span></div>
+              <div class="irs-cmmt-details2">
+                <h3>Extraordinary Faculty</h3>
+                <p>World-renowned researchers provide personal attention, with a 19:1 student-to-faculty ratio.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+  
+  <!-- Clients Section -->
+  <section class="irs-client">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center">
+          <div class="irs-clients-title">
+            <h2>Our Affilations with different organisation</h2>
+           
+      </div>
+      <div class="row irs-mrgnt">
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 clearfix animatedParent">
+            <div class="irs-clients-title text-center animated growIn"><img class="img-responsive" src="images/partner1.png" alt="2.png"></div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 clearfix animatedParent">
+            <div class="irs-clients-title text-center animated growIn"><img class="img-responsive" src="images/partner2.png" alt="3.png"></div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 clearfix animatedParent">
+            <div class="irs-clients-title text-center animated growIn"><img class="img-responsive" src="images/partner4.png" alt="4.png"></div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 clearfix animatedParent">
+            <div class="irs-clients-title text-center animated growIn"><img class="img-responsive" src="images/partner5.png" alt="5.png"></div>
+        </div>
+         
+      </div>
+    </div>
+  </section>
+  
+  <!-- Newsletter Section -->
+  <section class="irs-newsletter-subscribe">
+     
+       
+    </div>
+  </section>
+  
+   
+  
+  <!-- Our CopyRight Section -->
+  <section class="irs-copy-right">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>Copyright© 2017 All Rights Received. <i class="fa fa-book"></i> <span class="text-thm2">Tekriwal Education Pvt Ltd.</span></p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a> </div>
+<!-- Wrapper End --> 
+<script type="text/javascript" src="js/jquery.js"></script> 
+<script type="text/javascript" src="js/jquery-ui.min.js"></script> 
+<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="js/bootsnav.js"></script> 
+<script type="text/javascript" src="js/scrollto.js"></script> 
+<script type="text/javascript" src="js/jquery-scrolltofixed-min.js"></script> 
+<script type="text/javascript" src="js/jquery-SmoothScroll-min.js"></script> 
+<script type="text/javascript" src="js/jquery.counterup.js"></script> 
+<script type="text/javascript" src="js/fancybox.js"></script> 
+<script type="text/javascript" src="js/wow.min.js"></script> 
+<script type="text/javascript" src="js/jquery.masonry.min.js"></script> 
+<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script> 
+<script type="text/javascript" src="js/owl.carousel.min.js"></script> 
+<script type="text/javascript" src="js/jquery.fitvids.js"></script> 
+<script type="text/javascript" src="js/css3-animate-it.js"></script> 
+<script type="text/javascript" src="js/swiper.min.js"></script> 
+<script type="text/javascript" src="js/flipclock.min.js"></script> 
+<!-- Custom script for all pages --> 
+<script type="text/javascript" src="js/script.js"></script> 
+
+<!-- Initialize Swiper --> 
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        paginationClickable: true,
+        spaceBetween: 20,
+        mousewheelControl: true
+    });
+</script>
+</body>
+</html>
